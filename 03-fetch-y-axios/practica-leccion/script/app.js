724d96f63f691ef4bf8176d830b83876a0ad7225
Mor4n <anm0ran@outlook.com>
2026-04-30 03:29:50 -0700

    Practica 3- Añadido reproductor de musica y destructuración de objeto

--- 03-fetch-y-axios/practica-leccion/script/app.js
+++ 03-fetch-y-axios/practica-leccion/script/app.js
@@ -5,10 +5,11 @@ let btn_axios = document.querySelector("#axios-btn")
 
 let formulario = document.querySelector("#formulario")
 
+let reproductor = document.querySelector("#reproductor");
 
 
 function obtenerDatos(valor) {
-    //? Con encodeURIComponent hago que si escribo "Instant crush - Daft punk" se ponga algo como "Instant%20Crush%20-%20Daft%20Punk"
+    //? Con encodeURIComponent hago que si escribo "Instant crush - Daft punk" se ponga algo como "Instant%20Crush%20 - %20Daft%20Punk"
     
     let api =`https://itunes.apple.com/search?term=${encodeURIComponent(valor)}&media=music&entity=song&limit=1`;
     
@@ -16,8 +17,9 @@ function obtenerDatos(valor) {
     .then((respuesta) => respuesta.json())
     .then((datos)=>{
 
-        console.log(datos.results);
+        // console.log(datos.results); arreglo que en el indice 0 va a mandar un objeto
         
+        mostrarHTML(datos.results);
 
     }
     )
@@ -25,6 +27,20 @@ function obtenerDatos(valor) {
 
 
 }
+
+
+
+function mostrarHTML(objeto) {
+    console.log(objeto);
+    
+    const { artistName, collectionName, trackName, previewUrl, artworkUrl100, releaseDate } = objeto[0];
+    console.log(`${artistName},${ collectionName}, trackName, ${previewUrl}, artworkUrl100, releaseDate`);
+    
+    reproductor.src = previewUrl;
+
+
+}
+
 
 
 formulario.addEventListener("submit",(e)=>{
