95634065052ce41beb3c4fbaadb881dc4a9f5b25
Mor4n <anm0ran@outlook.com>
2026-04-30 06:40:32 -0700

    Practica 3 (era la 3 pasada tambien) - Diseño final, README y Añadido maaas o menos degradado con vibrant en base al color de portada)

--- 03-fetch-y-axios/README.md
+++ 03-fetch-y-axios/README.md
@@ -1,5 +1,11 @@
 
 # Lección 03 - Fetch y Axios: Consumo de APIs con Fetch y Axios
+"
+El método fetch es una herramienta esencial para cualquier desarrollador JavaScript que desee trabajar con datos externos. Ofrece una forma moderna, limpia y eficiente de realizar solicitudes HTTP. Aunque es importante manejar los errores correctamente y comprender sus limitaciones, fetch es una opción poderosa y flexible para cualquier proyecto web. Axios es una herramienta poderosa y flexible para manejar solicitudes HTTP en JavaScript. Su facilidad de uso y características avanzadas lo hacen ideal para una variedad de aplicaciones, desde consumo de APIs hasta sistemas complejos.
+
+El objetivo es que desarrolles las habilidades necesarias para realizar solicitudes HTTP y manejar datos obtenidos de APIs, utilizando tanto `fetch` como Axios. Además, practicarás el manejo de errores y la representación de datos en una interfaz gráfica sencilla.
+
+En este proyecto, crearás una aplicación web sencilla que permita obtener información de personajes de una API de tu elección (como la de Star Wars o Rick & Morty). La aplicación deberá mostrar los datos obtenidos en la interfaz de usuario y ofrecerá dos botones para realizar las mismas solicitudes, uno utilizando `fetch` y otro utilizando `axios`. Esto te permitirá comparar el uso de ambas herramientas."
 
 
 
@@ -9,23 +15,29 @@
 
 - **./practica-leccion/style.css**: Archivo CSS del proyecto, conteniendo los estilos del proyecto
 
-- **./practica-leccion/script/app.js**: Archivo de Javascript con la práctica realizada para este proyecto, tanto en consola como con interfaz con el HTML.
+- **./practica-leccion/script/app.js**: Archivo de Javascript con la práctica realizada para este proyecto
 
 
-- **./capturas/Captura1.png**: Captura de pantalla de HTML junto con el resultado del ejercicio en consola
-- **./capturas/Captura2.png**: Captura de pantalla del HTML con el resultado de la palabra más larga
+- **./capturas/Captura1.png**: Captura de pantalla de HTML inicial
+- **./capturas/Captura2.png**: Captura de pantalla del HTML con fetch 
+- **./capturas/Captura3.png**: Captura de pantalla del HTML con axios
 
 
 ## Aprendizajes:
 
--
+- Obtención de datos de API's con:
+    - Fetch
+    - Axios
+- Aplicación de CDN's como Axios, videojs y node-vibrant
 
 
 ## Evidencia visual
 
 A continuación se muestra una captura de pantalla del código funcionando en la consola del navegador:
 
-![Captura inicial del programa](./capturas/Captura1.png)
+![Captura de pantalla de HTML inicial](./capturas/Captura1.png)
+![Captura de pantalla del HTML con fetch](./capturas/Captura2.png)
+![Captura de pantalla del HTML con axios](./capturas/Captura3.png)
 
 
 
@@ -42,11 +54,21 @@ dentro de su editor de código preferido o dentro de Github.
 ## Despliegue
 
 Se desplegó en Github Pages a partir de este repositorio, puedes ver la página a través del siguiente link:
-
+https://mor4n.github.io/05-desarrollo-avanzado-en-javascript/03-fetch-y-axios/practica-leccion/index.html
 
 
 ## Como conclusión personal:
-
+En esta práctica pude aprender algo que realmente, no se porque pensé que era tan complicado, que era el obtener datos a través de una API.
+En este caso, quise usar la API de Apple Music, ya que viendo la misma en su página, ví que existía la posibilidad de obtener un pedacito de la canción, con esto me imaginé de que "le voy a tratar de poner un mini reproductor a ver si funciona".
+Entonces, busqué en internet cdn's de reproductores de video, ya que quería continuar usando cdns así como usamos el axios, en lugar de instalarlo con node.
+Fue bastante genial el poder aprender sobre ambos, tanto fetch y axios, con fetch es un poquito más de texto y de conversión que con axios, pero siento que ambos se me hicieron muy buenos para usar en esta práctica :'D, hice varias funciones, tanto para obtener los datos ya sea con fetch o axios, para recorrer el arreglo de objetos de la respuesta y mostrarlos en el HTML, estoy intentando usar la destructuración cuando puedo y me gustó que pude aplicarlo aquí ;w; 
+Ya cuando iba terminando, quise ponerle un degradado al fondo como en spotify y encontré varios cdn's que hacen esto, entre ellos Gradent.js y Vibrant.js, probé ambos (el primero no me funcionó) y el segundo fue el que vine usando finalmente
+Al final de todo se le colocó un diseño tipo tarjetitas con los resultados que se obtuvieron n-n
 
 ## Fuentes:
+[API de Apple music](https://performance-partners.apple.com/search-api)
+
+[Reproductor de video/mp3](https://videojs.org/)
+
+[Generar gradientes](https://vibrant.dev/)
 
--- 03-fetch-y-axios/practica-leccion/script/app.js
+++ 03-fetch-y-axios/practica-leccion/script/app.js
@@ -1,112 +1,148 @@
 
-let input_nombre = document.querySelector("#input_nombre")
-let btn_fetch = document.querySelector("#fetch-btn")
-let btn_axios = document.querySelector("#axios-btn")
+    let input_nombre = document.querySelector("#input_nombre")
+    let btn_fetch = document.querySelector("#fetch-btn")
+    let btn_axios = document.querySelector("#axios-btn")
 
-let formulario = document.querySelector("#formulario")
+    let formulario = document.querySelector("#formulario")
 
-let reproductor = document.querySelector("#reproductor");
-let imagen_reproductor = document.querySelector("#imagen-reproductor");
+    let reproductor = document.querySelector("#reproductor");
+    let imagen_reproductor = document.querySelector("#imagen-reproductor");
 
-let data_container = document.querySelector(".data-container")
-
-
- function  axiosObtenerDatos (valor) {
-    let api =`https://itunes.apple.com/search?term=${encodeURIComponent(valor)}&media=music&entity=song&limit=3`;
-    
-
-     axios.get(api)
-    .then( respuesta => recorrerRespuesta(respuesta.data.results))
+    let data_container = document.querySelector(".data-container")
 
 
-}
-
-
-function fetchObtenerDatos(valor) {
-    //? Con encodeURIComponent hago que si escribo "Instant crush - Daft punk" se ponga algo como "Instant%20Crush%20 - %20Daft%20Punk"
-    
-    let api =`https://itunes.apple.com/search?term=${encodeURIComponent(valor)}&media=music&entity=song&limit=3`;
-    
-    fetch(api)
-    .then((respuesta) => respuesta.json())
-    .then((datos)=>{
-
-        // console.log(datos.results); arreglo que en el indice 0 va a mandar un objeto
+    function  axiosObtenerDatos (valor) {
+        let api =`https://itunes.apple.com/search?term=${encodeURIComponent(valor)}&media=music&entity=song&limit=3`;
         
-        recorrerRespuesta(datos.results);
+
+        axios.get(api)
+        .then( respuesta => recorrerRespuesta(respuesta.data.results))
+
 
     }
-    )
 
+
+    function fetchObtenerDatos(valor) {
+        //? Con encodeURIComponent hago que si escribo "Instant crush - Daft punk" se ponga algo como "Instant%20Crush%20 - %20Daft%20Punk"
+        
+        let api =`https://itunes.apple.com/search?term=${encodeURIComponent(valor)}&media=music&entity=song&limit=3`;
+        
+        fetch(api)
+        .then((respuesta) => respuesta.json())
+        .then((datos)=>{
+
+            // console.log(datos.results); arreglo que en el indice 0 va a mandar un objeto
+            
+            recorrerRespuesta(datos.results);
+
+        }
+        )
+
+    }
+
+
+
+    function recorrerRespuesta(respuesta){
+        console.log(respuesta);
+        
+        data_container.innerHTML = ``; // reset de lo que esté dentro del contenedor
+
+        respuesta.forEach(elemento => {
+            mostrarHTML(elemento);
+        });
+
+        aplicarDegradadoFondo();
+
+
+    }
+
+    function aplicarDegradadoFondo() {
+    const img = document.querySelector('.cover-img');
+
+    if (!img) return;
+
+    // si la imagen ya terminó de cargarse (.complete), tipo ya cargó o nel?
+    if (img.complete) {
+        generarGradiente(img);
+    } else {
+        // estado de una imagen como tipo eh avisame cuando ya cargue la imagen
+        img.onload = () => generarGradiente(img);
+    }
 }
 
+async function generarGradiente(img) {
+    const palette = await Vibrant.from(img.src).getPalette();
 
+    const color1 = palette.LightVibrant?.hex || palette.LightMuted?.hex || "#888";
+    const color2 = palette.Vibrant?.hex || palette.Muted?.hex || "#555";
+    const color3 = palette.DarkVibrant?.hex || palette.DarkMuted?.hex || "#222";
 
-function recorrerRespuesta(respuesta){
-    console.log(respuesta);
-    
-    data_container.innerHTML = ``; // reset de lo que esté dentro del contenedor
-
-    respuesta.forEach(elemento => {
-        mostrarHTML(elemento);
-    });
-
-}
-
-
-function mostrarHTML(objeto) {
-    console.log(objeto);
-
-    const { artistName, collectionName, trackName, previewUrl, artworkUrl100, releaseDate } = objeto;
-
-    
-
-    const reproductor_HTML = `
-        
-        <div class="info-obtenida" id="contenedor-reproductor">
-            <video-player>
-            <video-minimal-skin>
-
-            <media-poster>
-                <img src="${artworkUrl100}" id="imagen-reproductor"/>
-            </media-poster>
-
-                <video src="${previewUrl}" playsinline id="reproductor"></video>
-            </video-minimal-skin>
-            </video-player>
-        </div>
-
-        <div class="" id="contenedor-info">
-            <h2>Canción: <span>${trackName}</span></h2>
-            <h2>Artista: <span>${artistName}</span></h2>
-            <h2>Álbum: <span>${collectionName}</span></h2>
-            <h2>Fecha de lanzamiento: <span>${releaseDate}</span></h2>
-
-        </div>
-
-        
+    document.body.style.background = `
+        linear-gradient(135deg, ${color1}, ${color2}, ${color3})
     `;
-
-
-
-    data_container.innerHTML +=reproductor_HTML; // añado los datos
-
-
-
-
 }
 
 
+    function mostrarHTML(objeto) {
+        console.log(objeto);
 
-btn_fetch.addEventListener("click",(e)=>{
-    e.preventDefault();
-    fetchObtenerDatos(input_nombre.value);
-})
+        const { artistName, collectionName, trackName, previewUrl, artworkUrl100, releaseDate } = objeto;
+
+        const imagenAltaResolucion = artworkUrl100.replace("100x100", "300x300");
+
+        const reproductor_HTML = `
+
+            <article class="tarjeta-cancion">
+                <section class="tarjeta-cancion-info">
+                    <p class="tarjeta-cancion-cancion">Canción</p>
+                    <h2>${trackName}</h2>
+                    <p><span>Artista:</span> ${artistName}</p>
+                    <p><span>Álbum:</span> ${collectionName}</p>
+                    <p><span>Lanzamiento:</span> ${releaseDate}</p>
+                </section>
+
+                <section class="tarjeta-cancion-reproductor">
+                    <div class="imagen-degradado">
+                        <img src="${imagenAltaResolucion}" class="cover-img" alt="Portada de ${trackName}"/>
+                    </div>
+
+                    <div class="info-obtenida" id="contenedor-reproductor">
+                        <video-player>
+                            <video-minimal-skin>
+                                <media-poster>
+                                    <img src="${imagenAltaResolucion}" id="imagen-reproductor" alt="Portada de ${trackName}"/>
+                                </media-poster>
+
+                                <video src="${previewUrl}" playsinline id="reproductor"></video>
+                            </video-minimal-skin>
+                        </video-player>
+                    </div>
+                </section>
+            </article>
+
+            
+        `;
 
 
 
-btn_axios.addEventListener("click",(e)=>{
-    e.preventDefault();
-    axiosObtenerDatos(input_nombre.value);
-})
+        data_container.innerHTML +=reproductor_HTML; // añado los datos
+
+
+
+
+    }
+
+
+
+    btn_fetch.addEventListener("click",(e)=>{
+        e.preventDefault();
+        fetchObtenerDatos(input_nombre.value);
+    })
+
+
+
+    btn_axios.addEventListener("click",(e)=>{
+        e.preventDefault();
+        axiosObtenerDatos(input_nombre.value);
+    })
 
