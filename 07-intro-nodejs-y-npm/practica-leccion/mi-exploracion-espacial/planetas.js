d85e28fcf0a12045347d04bf76375bb25374a1a5
Mor4n <anm0ran@outlook.com>
2026-05-05 20:28:50 -0700

    Practica 7 - Añadidos planetas

---
+++ 07-intro-nodejs-y-npm/practica-leccion/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,32 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Viltrum",
+    descripcion: "Un planeta de guerreros superpoderosos.",
+    descubiertoEn: "Desconocido"
+  },
+  {
+    nombre: "Namek",
+    descripcion: "Un planeta verde habitado por los Namekianos.",
+    descubiertoEn: "Desconocido"
+  },
+  {
+    nombre: "Johto",
+    descripcion: "Una de las mejores regiones del mundo Pokémon porque existe Chikorita y Cyndaquil.",
+    descubiertoEn: "1996"
+  },
+  {
+    nombre: "Greed Island",
+    descripcion: "Un juego de realidad virtual que se convierte en un planeta, ocupas una tarjeta para ingresar al juego (hunter x hunter)",
+    descubiertoEn: "2000"
+  }
+];
