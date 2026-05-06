4dc792dfc6e5eb65869875ce3aa585d08f8c713c
Mor4n <anm0ran@outlook.com>
2026-05-05 20:45:07 -0700

    Practica 7 - Añadidos datos adicionales

--- 07-intro-nodejs-y-npm/practica-leccion/mi-exploracion-espacial/index.js
+++ 07-intro-nodejs-y-npm/practica-leccion/mi-exploracion-espacial/index.js
@@ -4,5 +4,7 @@ planetas.forEach(planeta => {
   console.log(`El planeta: ${planeta.nombre} ha sido descubierto!`);
   console.log(`Un reporte nos dice que...: ${planeta.descripcion}`);
   console.log(`Al parecer, fue descubierto en: ${planeta.descubiertoEn}`);
+  console.log(`He obtenido una imagen de como se ve!!: ${planeta.img}`);
+  console.log(`A lo que puedo localizar, sus coordenadas más o menos son: ${ planeta.coordenadas.length === 0 ? "desconocidas... sus ondas magnéticas me impiden obtener correctamente sus coordenadas :( " : planeta.coordenadas.join(', ') }`);
   console.log('---');
 });
--- 07-intro-nodejs-y-npm/practica-leccion/mi-exploracion-espacial/planetas.js
+++ 07-intro-nodejs-y-npm/practica-leccion/mi-exploracion-espacial/planetas.js
@@ -2,32 +2,44 @@ const planetas = [
   {
     nombre: "Titán",
     descripcion: "La luna más grande de Saturno, con lagos de metano.",
-    descubiertoEn: "1655"
+    descubiertoEn: "1655",
+    img:"https://upload.wikimedia.org/wikipedia/commons/4/45/Titan_in_true_color.jpg",
+    coordenadas:["45707.3:226384.5:5796384.5","36384.5:216950.5:5796384.5","36384.5:235677.6:5796384.5"]
   },
   {
     nombre: "Próxima Centauri b",
     descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
-    descubiertoEn: "2016"
+    descubiertoEn: "2016",
+    img:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Artist%27s_impression_of_the_planet_orbiting_Proxima_Centauri.jpg/1920px-Artist%27s_impression_of_the_planet_orbiting_Proxima_Centauri.jpg",
+    coordenadas:["45707.3:226384.5:5796384.5","36384.5:216950.5:5796384.5","36384.5:235677.6:5796384.5"]
   },
   {
     nombre: "Viltrum",
     descripcion: "Un planeta de guerreros superpoderosos.",
-    descubiertoEn: "Desconocido"
+    descubiertoEn: "Desconocido",
+    img:"https://static.wikia.nocookie.net/amazon-invincible/images/3/34/Viltrum_Ring_Planet.png/revision/latest/scale-to-width-down/1000?cb=20260423113252",
+    coordenadas:["15234.1:83452.9:472819.3", "16234.1:84452.9:482819.3"]
   },
   {
-    nombre: "Namek",
+    nombre: "Namekusei",
     descripcion: "Un planeta verde habitado por los Namekianos.",
-    descubiertoEn: "Desconocido"
+    descubiertoEn: "Desconocido",
+    img:"https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/02/namek-dragon-ball.jpg?resize=1280%2C630&ssl=1",
+    coordenadas:["45321.8:77432.1:223849.5", "46321.8:78432.1:233849.5"]
   },
   {
     nombre: "Johto",
     descripcion: "Una de las mejores regiones del mundo Pokémon porque existe Chikorita y Cyndaquil.",
-    descubiertoEn: "1996"
+    descubiertoEn: "1996",
+    img:"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fe/latest/20180203062443/Johto_HGSS.png/800px-Johto_HGSS.png?20180203062443",
+    coordenadas:[]
   },
   {
     nombre: "Greed Island",
     descripcion: "Un juego de realidad virtual que se convierte en un planeta, ocupas una tarjeta para ingresar al juego (hunter x hunter)",
-    descubiertoEn: "2000"
+    descubiertoEn: "2000",
+    img:"https://cdn.shopify.com/s/files/1/0252/1736/8154/files/EHgDjblXkAItqnY_480x480.jpg?v=1630404597",
+    coordenadas:[]
   }
 ];
 
