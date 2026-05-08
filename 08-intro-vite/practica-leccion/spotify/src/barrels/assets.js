a7452afcf007608d06dee2b6165d16c939358af0
Mor4n <anm0ran@outlook.com>
2026-05-08 07:09:40 -0700

    Proyecto final - Navbar completado

---
+++ 08-intro-vite/practica-leccion/spotify/src/barrels/assets.js
@@ -0,0 +1,72 @@
+
+import workout from "../assets/workout.png"
+import megusta from "../assets/megusta.png"
+import logo from "../assets/logo.png"
+import kindie from "../assets/k-indie.png"
+import home from "../assets/home.png"
+import descarga from "../assets/descarga.png"
+import descargacompleto from "../assets/descarga-completo.png"
+import avatar from "../assets/avatar.jpg"
+import anterior from "../assets/barra-reproduccion/anterior.png"
+import barra from "../assets/barra-reproduccion/barra.png"
+import bucle from "../assets/barra-reproduccion/bucle.png"
+import play from "../assets/barra-reproduccion/play.png"
+import random from "../assets/barra-reproduccion/random.png"
+import siguiente from "../assets/barra-reproduccion/siguiente.png"
+import volumen from "../assets/barra-reproduccion/volumen.png"
+import aimer from "../assets/estaciones/aimer.jpg"
+import joji from "../assets/estaciones/joji.jpg"
+import nujabes from "../assets/estaciones/nujabes.jpg"
+import radwimps from "../assets/estaciones/radwimps.jpg"
+import strokes from "../assets/estaciones/strokes.jpg"
+import descubrimiento from "../assets/hecho-para/descubrimiento.jpg"
+import mixDiario1 from "../assets/hecho-para/mix-diario-1.jpg"
+import mixDiario2 from "../assets/hecho-para/mix-diario-2.jpg"
+import mixDiario3 from "../assets/hecho-para/mix-diario-3.jpg"
+import mixDiario4 from "../assets/hecho-para/mix-diario-4.jpg"
+import mixDiario5 from "../assets/hecho-para/mix-diario-5.jpg"
+import tresTresTres from "../assets/tarjetita-main/333.webp"
+import cancionesImpactan from "../assets/tarjetita-main/canciones-impactan.webp"
+import forge from "../assets/tarjetita-main/forge.jpg"
+import kindieTarjetita from "../assets/tarjetita-main/k-indie.webp"
+import meRecuerdaA from "../assets/tarjetita-main/me-recuerda-a.webp"
+import queMeRecuerda from "../assets/tarjetita-main/que-me-recuerda.webp"
+import sime from "../assets/tarjetita-main/sime.webp"
+import workoutTarjetita from "../assets/tarjetita-main/workout.webp"
+
+export {
+    avatar,
+	workout,
+	megusta,
+	logo,
+	kindie,
+	home,
+	descarga,
+	descargacompleto,
+	anterior,
+	barra,
+	bucle,
+	play,
+	random,
+	siguiente,
+	volumen,
+	aimer,
+	joji,
+	nujabes,
+	radwimps,
+	strokes,
+	descubrimiento,
+	mixDiario1,
+	mixDiario2,
+	mixDiario3,
+	mixDiario4,
+	mixDiario5,
+	tresTresTres,
+	cancionesImpactan,
+	forge,
+	kindieTarjetita,
+	meRecuerdaA,
+	queMeRecuerda,
+	sime,
+	workoutTarjetita,
+}
--- 08-intro-vite/practica-leccion/spotify/src/components/navbar.js
+++ 08-intro-vite/practica-leccion/spotify/src/components/navbar.js
@@ -1,7 +1,20 @@
+import  { logo,home, avatar } from "../barrels/assets"
 
 const Navbar = () =>{
 
     return /* html */`
+    <nav>
+        <img src="${logo}" id="logo">
+        
+        <div id="home-search">
+            <img src="${home}" alt="Icono de home" id="home" />
+            <div id="search-container">
+                <input type="text" id="search" placeholder="¿Qué quieres reproducir?"/>
+            </div>
+        </div>
+        <img src="${avatar}" id="avatar">
+
+    </nav>
     `
 }
 
